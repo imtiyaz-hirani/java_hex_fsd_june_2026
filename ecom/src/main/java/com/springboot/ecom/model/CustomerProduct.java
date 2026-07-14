1f75daf4a7cd50d4a37e29f9f908cf27e56a00bc
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-07-14 15:21:13 +0530

    CustomerProduct i.e. Order model added

---
+++ ecom/src/main/java/com/springboot/ecom/model/CustomerProduct.java
@@ -0,0 +1,34 @@
+package com.springboot.ecom.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "customer_product")
+public class CustomerProduct { // This can also be an Order table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreationTimestamp
+    private Instant purchaseDate;
+
+    private int qty;
+    private double discount;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id" , nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id" , nullable = false)
+    private Product product;
+}
