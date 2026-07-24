13ea54ff7ef6127707120c4ac7efc45f40b8b098
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-07-24 15:10:45 +0530

    app settings + state variables

---
+++ react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import CountOp from "./components/countOp"
+
+ function App() { // Component - Base
+
+  return ( 
+    <div>
+       <CountOp />
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/src/components/CountOp.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"; //<-- this is a React hook 
+
+function CountOp(){
+
+    let count = 0; // Normal -- React won't track it. on update, it wont show latest value
+    const [likeCount, setLikeCount] = useState(0)
+
+    const incrCount = ()=>{
+        setLikeCount(likeCount + 1)
+    }
+
+    const decrCount = ()=>{
+        if(likeCount === 0)
+            return
+
+        setLikeCount(likeCount - 1)
+    }
+    return(
+        <div>
+            <h1>My Count Component</h1>
+            <button onClick={()=>incrCount()}>Like </button>
+             <button onClick={()=>decrCount()}>Dis-Like</button>
+             {likeCount}
+        </div>
+    )
+}
+
+export default CountOp
