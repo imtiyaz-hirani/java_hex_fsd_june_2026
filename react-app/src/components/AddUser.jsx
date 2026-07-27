cd0c57b5b5fdec55d0da1d5b9c36ca6c7b6a9f47
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2026-07-27 12:02:19 +0530

    array-push

--- react-app/src/components/AddUser.jsx
+++ react-app/src/components/AddUser.jsx
@@ -2,15 +2,30 @@ import { useState } from "react"
 
 function AddUser() {
 
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [city,setCity] = useState('')
-
-    const add = ($event)=>{
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [city, setCity] = useState('')
+    const [customers, setCustomers] = useState([])
+    const [temp, setTemp] = useState([])
+    const add = ($event) => {
         $event.preventDefault() // It tells web browser to not refresh the page on form submit
-        console.log(name)
-        console.log(email)
-        console.log(city)
+        // create an object of customer 
+        let customer = {
+            'name': name,
+            'email': email,
+            'city': city
+        }
+         
+        //push the object in the array
+        // customers.push(customer)
+        // setCustomers([...customers])
+        temp.push(customer)
+        setCustomers([...temp])
+    }
+
+    const onDelete = (cemail)=>{
+        // To delete, filter the array
+        setCustomers([...customers.filter((c)=> c.email !== cemail)])
     }
     return (
         <div>
@@ -19,14 +34,14 @@ function AddUser() {
                     <div className="col-md-6">
                         <div className="card">
                             <div className="card-body">
-                                <form onSubmit={($event)=> add($event)}>
+                                <form onSubmit={($event) => add($event)}>
                                     <div className="row">
                                         <div className="col-sm-4" style={{ 'textAlign': 'right' }}>
                                             <label>Enter Name: </label>
                                         </div>
                                         <div className="col-md-8">
                                             <input type="text" className="form-control" required
-                                            onChange={($event)=> setName($event.target.value)} />
+                                                onChange={($event) => setName($event.target.value)} />
                                         </div>
                                     </div>
                                     <div className="row mt-4">
@@ -34,8 +49,8 @@ function AddUser() {
                                             <label>Enter Email: </label>
                                         </div>
                                         <div className="col-md-8">
-                                            <input type="email" className="form-control" required 
-                                            onChange={($event)=> setEmail($event.target.value)}/>
+                                            <input type="email" className="form-control" required
+                                                onChange={($event) => setEmail($event.target.value)} />
                                         </div>
                                     </div>
                                     <div className="row mt-4">
@@ -43,8 +58,8 @@ function AddUser() {
                                             <label>Select City: </label>
                                         </div>
                                         <div className="col-md-8">
-                                            <select onChange={($event)=> setCity($event.target.value)} 
-                                            className="form-control" required>
+                                            <select onChange={($event) => setCity($event.target.value)}
+                                                className="form-control" required>
                                                 <option>--select city--</option>
                                                 <option>Mumbai</option>
                                                 <option>Chennai</option>
@@ -61,8 +76,35 @@ function AddUser() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        here, do anything
+                    <div className="col-md-6" style={{ 'textAlign': 'center' }}>
+                        <h3>All Customers </h3>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">City</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    customers.map((c,index) => (
+                                        <tr key={index}>
+                                            <th scope="row">{index+1}</th>
+                                            <td>{c.name}</td>
+                                            <td>{c.email}</td>
+                                            <td>{c.city}</td>
+                                            <td><i className="bi bi-trash" onClick={()=>onDelete(c.email)}></i></td>
+                                        </tr>
+                                    ))
+                                }
+
+
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
             </div>
